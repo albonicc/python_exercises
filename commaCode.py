edb92e1a27d6235a8f0fa956cfcfa5af5b7e65b6
albonicc <nikygam@gmail.com>
2020-04-25 00:07:06 -0500

    New codes added

---
+++ commaCode.py
@@ -0,0 +1,20 @@
+def insertComa(list):
+    for i in range(len(list)):
+        if i < len(list) - 2: 
+            list[i] = list[i] + ", "
+        elif i == len(list) - 2:
+            list[i] = list[i] + " "
+        else:
+            list[i] = "and " + list[i]
+
+def toStr(list):
+    string = ""
+    for i in range(len(list)):
+        string += list[i]
+    return string
+
+spam = ['apples', 'bananas', 'tofu', 'cats']
+insertComa(spam)
+result = toStr(spam)
+print(result)
+ 
