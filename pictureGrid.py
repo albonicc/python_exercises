f8abc9a13e887c9648458073ac4a5a0dbd83e872
albonicc <nikygam@gmail.com>
2020-04-26 00:36:15 -0500

    Just solved pictureGrid

---
+++ pictureGrid.py
@@ -0,0 +1,23 @@
+def transform(matrix):
+    rows = range(len(matrix[0]))
+    columns = range(len(matrix))
+    for i in rows:
+        for j in columns:
+            print(matrix[j][i], end="")
+        print("")
+
+
+grid = [['.', '.', '.', '.', '.', '.'],
+        ['.', 'O', 'O', '.', '.', '.'],
+        ['O', 'O', 'O', 'O', '.', '.'],
+        ['O', 'O', 'O', 'O', 'O', '.'],
+        ['.', 'O', 'O', 'O', 'O', 'O'],
+        ['O', 'O', 'O', 'O', 'O', '.'],
+        ['O', 'O', 'O', 'O', '.', '.'],
+        ['.', 'O', 'O', '.', '.', '.'],
+        ['.', '.', '.', '.', '.', '.']]
+
+
+transform(grid)
+
+
