1b32ad5a9a1c22f3e24fd5ac9e34f88de0815d0a
albonicc <nikygam@gmail.com>
2020-04-28 00:07:29 -0500

    Just finished all the exercises of the chapter 5 of the book

---
+++ inventoryDragon.py
@@ -0,0 +1,22 @@
+dragonLoot = ['gold coin', 'dagger', 'gold coin', 'gold coin', 'ruby']
+
+def addToInventory(inventory, addedItems):
+    for item in addedItems:
+        if item in inventory.keys():
+            inventory[item] += 1
+        else:
+            inventory.setdefault(item, 1)
+    return inventory 
+
+def displayInventory(inventory):
+    print("Inventory:")
+    item_total = 0
+    for k, v in inventory.items():
+        print(str(v) + " " + k)
+        item_total += v
+    print("Total number of items: " + str(item_total))
+
+inv = {'gold coin': 42, 'rope': 1}
+dragonLoot = ['gold coin', 'dagger', 'gold coin', 'gold coin', 'ruby']
+inv = addToInventory(inv, dragonLoot)
+displayInventory(inv)
